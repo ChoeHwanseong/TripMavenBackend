af9e339990f680abb94bbea5e45b00d23bf04987
ChoiGaHeun <aprilcgh@naver.com>
2024-09-19 11:45:19 +0900

    커밋

--- src/main/java/com/tripmaven/members/service/MembersService.java
+++ src/main/java/com/tripmaven/members/service/MembersService.java
@@ -199,7 +199,7 @@ public class MembersService {
     }
 
     
-    /////가나다라마ㅁ바사이메일인증인증인증야야야아여여아야
+   
     public EmailVerificationResult verifiedCode(String email, String authCode) {
         this.checkDuplicatedEmail(email);
         String redisAuthCode = redisService.getData(AUTH_CODE_PREFIX + email);
